7a42e2bbb947326984f00350926a2756593e060c
apzzd <ada@dyer.me>
2020-07-15 13:52:15 -0400

    updated webpg

--- cybersecurity2/main.py
+++ cybersecurity2/main.py
@@ -12,11 +12,14 @@ alert("WARNING: I've got some of your information already!\n Language: " + docum
 def click(ev):
 	if document['name'].value and document['age'].value and document['anim'].value and document['hobb'].value:
 		alert('Hey: I can save this information, you know. Now I know that your name is ' + document['name'].value + ' and that you are ' + document['age'].value + ' years old.')
+		showunames()
+		showpass()
+		document.getElementById("myDIV").style.visibility = 'visible'
 	else:
 		alert('You must fill out the form first.')
 	
 
-def showunames(ev):
+def showunames():
 	if document['name'].value and document['age'].value and document['hobb'].value and document['anim'].value and document['color'].value and document['food'].value:
 		un1 = document['name'].value + document['anim'].value + document['age'].value + str(randint(0,9)) + str(randint(0,9))
 		un2 = document['hobb'].value + document['name'].value + document['age'].value + str(randint(0,9)) + str(randint(0,9))
@@ -25,11 +28,11 @@ def showunames(ev):
 		un5 = document['color'].value + document['anim'].value + document['age'].value + str(randint(0,9)) + str(randint(0,9))
 		un6 = document['anim'].value + document['food'].value + document['age'].value + str(randint(0,9)) + str(randint(0,9))
 		
-		document['unames'] <= 'Usernames: ' + un1 + ' | ' + un2 + ' | ' + un3 + ' | ' + un4 + ' | ' + un5 + ' | ' + un6
+		document['unames'] <= un1 + ' | ' + un2 + ' | ' + un3 + ' | ' + un4 + ' | ' + un5 + ' | ' + un6
 	else:
 		alert('You must fill out the form first')
 		
-def showpass(ev):
+def showpass():
 	adj = ['cool', 'cute', 'fluffy', 'evil', 'awesome', 'purple', 'orange']
 	nouns = ['pandas', 'kittenz', 'manat33s', 'crocodiles', 'doggos', 'id3as', 'toes']
 	noun = ['panda', 'kitten', 'manat33', 'crocodile', 'doggo', 'id3a', 'toe']
@@ -44,7 +47,7 @@ def showpass(ev):
 	p5 = choice(number) + choice(adj) + choice(nouns) + choice(verb) + choice(nouns)
 	p6 = choice(number) + choice(adj) + choice(food) + choice(nouns)
 	
-	document['pass'] <= 'Passwords: ' + p1 + ' | ' + p2 + ' | ' + p3 + ' | ' + p4 + ' | ' + p5 + ' | ' + p6
+	document['pass'] <= p1 + ' | ' + p2 + ' | ' + p3 + ' | ' + p4 + ' | ' + p5 + ' | ' + p6
  
 def uselect(ev):
 	##alert(ev.target)
@@ -59,7 +62,5 @@ def pselect(ev):
 
 # bind event 'click' on button to function echo
 document["submit"].bind("click", click)
-document["shownamez"].bind("click", showunames)
-document['showpass'].bind('click', showpass)
 document['unames'].bind('copy', uselect)
 document['pass'].bind('copy', pselect)
